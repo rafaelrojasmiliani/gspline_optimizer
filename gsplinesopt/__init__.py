172172468d7222cddb0a2c12b06b5680accdab99
Rafael Rojas <rafael.rojas@fraunhofer.it>
2021-01-20 10:08:43 +0100

    [*] functions added in __init__

---
+++ gsplinesopt/__init__.py
@@ -0,0 +1,2 @@
+from .ipopt import minimum_jerk_path
+from .ipopt import minimum_weighed_speed_jerk_path
---
+++ gsplinesopt/ipopt/__init__.py
@@ -0,0 +1,2 @@
+from .optpath import minimum_jerk_path
+from .optpath import minimum_weighed_speed_jerk_path
